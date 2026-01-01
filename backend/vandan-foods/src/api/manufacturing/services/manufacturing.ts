7f3a340b95962832072709ccb1303af48103561c
Ishani <ishanik@addact.net>
2026-01-01 14:21:15 +0530

    feature/page-structure
    I Have Added below pages
    
    Product Page
    Home
    About
    About Company
    Contacts Us
    Investor
    Management
    Manufacturing
    Marketing
    Our Board Of Director
    Our Business Strategy
    Quality Certification
    Real Ingredient
    Page Not Fount

---
+++ backend/vandan-foods/src/api/manufacturing/services/manufacturing.ts
@@ -0,0 +1,7 @@
+/**
+ * manufacturing service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::manufacturing.manufacturing');
---
+++ backend/vandan-foods/src/api/our-board-of-director/services/our-board-of-director.ts
@@ -0,0 +1,7 @@
+/**
+ * our-board-of-director service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::our-board-of-director.our-board-of-director');
